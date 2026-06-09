d8de83eb3befd330761086bba0027733bcb39a0a
rajat-jadav <rajatjadav39@gmail.com>
2026-06-09 12:47:03 +0530

    Program 3

---
+++ p3.php
@@ -0,0 +1,14 @@
+<?php
+
+$num1 = 10;
+$num2 = 5;
+
+echo "----- Arithmetic Operators -----<br><br   >";
+echo "Addition: " . ($num1 + $num2) . "<br>";
+echo "Subtraction: " . ($num1 - $num2) . "<br>";
+echo "Multiplication: " . ($num1 * $num2) . "<br>";
+echo "Division: " . ($num1 / $num2) . "<br>";
+echo "Modulus: " . ($num1 % $num2) . "<br>";
+echo "Exponentiation: " . ($num1 ** $num2) . "<br><br>";
+
+?>
