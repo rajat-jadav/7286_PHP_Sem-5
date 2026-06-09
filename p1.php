1a684448bbdcbef9f72c4a2c5526424856419239
rajat-jadav <rajatjadav39@gmail.com>
2026-06-09 12:19:41 +0530

    First Program

---
+++ p1.php
@@ -0,0 +1,16 @@
+<?php
+
+$s1 = 89;
+$s2 = 90;
+$s3 = 91;
+$s4 = 92;
+$s5 = 93;
+
+
+$res = "PASS";
+
+echo "Marks::<br>PYTHON: $s1<br>C#: $s2<br>Java: $s3<br>JavaScript: $s4<br>PHP: $s5<br>";
+echo "TOTAL MARKS: " . ($s1 + $s2 + $s3 + $s4 + $s5) . "<br>";
+echo "Result: $res";
+
+?>
