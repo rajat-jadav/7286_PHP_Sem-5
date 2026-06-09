b3903fc52ada704536f74c86f6ddee688784782f
rajat-jadav <rajatjadav39@gmail.com>
2026-06-09 13:00:17 +0530

    CHANGES

--- p1.php
+++ p1.php
@@ -12,6 +12,5 @@ $res = "PASS";
 echo "Marks::<br>PYTHON: $s1<br>C#: $s2<br>Java: $s3<br>JavaScript: $s4<br>PHP: $s5<br>";
 echo "TOTAL MARKS: " . ($s1 + $s2 + $s3 + $s4 + $s5) . "<br>";
 echo "Result: $res";
-echo "ABCD";
 
 ?>
