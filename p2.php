4cda87eb94c23465a6156a3c2f53bd3eedf99f3a
rajat-jadav <rajatjadav39@gmail.com>
2026-06-09 12:39:15 +0530

    changes

--- p2.php
+++ p2.php
@@ -11,4 +11,14 @@ if ($num1 > $num2) {
     echo "$num1 is equal to $num2";
 }
 
+echo "<br>";
+
+if ($num1 < $num2) {
+    echo "$num1 is less than $num2";
+} elseif ($num1 > $num2) {
+    echo "$num1 is greater than $num2";
+} else {
+    echo "$num1 is equal to $num2";
+}
+
 ?>
